8b7b023689abdb5e2a4128737e03b536522704e9
new92 <new92github@gmail.com>
2023-10-29 16:26:38 +0200

    Create fib.c

---
+++ fibonacci_sequence/fib.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int n) {
+    int a = 0, b = 1, next, i;
+
+    if (n <= 0)
+        return a;
+
+    for (i = 2; i <= n; i++) {
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <n>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+
+    if (n < 0) {
+        printf("Fibonacci sequence is not defined for negative numbers.\n");
+        return 1;
+    }
+
+    int result = fibonacci(n);
+    printf("The %dth Fibonacci number is: %d\n", n, result);
+
+    return 0;
+}
